4944bd4e04bdd3721a5526f8d543c299f2cd60c7
Chris J. Karr <chris@audacious-software.com>
2020-10-16 03:10:04 +0000

    Initial code commit

---
+++ client.py
@@ -0,0 +1,489 @@
+# pylint: disable=line-too-long, no-member
+# -*- coding: utf-8 -*-
+
+
+
+from builtins import str
+from builtins import object
+import datetime
+import json
+import logging
+import time
+
+from enum import Enum
+
+import arrow
+import requests
+
+class HiveError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+def post_request_with_retries(url, payload, logger, max_retry_duration=120, initial_retry_duration=3.75, server_timeout=60):
+    query = None
+    last_error = None
+
+    while initial_retry_duration <= max_retry_duration:
+        try:
+            query = None
+
+            if server_timeout is not None:
+                query = requests.post(url, data=payload, timeout=server_timeout)
+            else:
+                query = requests.post(url, data=payload)
+                
+            if query.status_code == requests.codes.ok:
+                return query
+            elif query.status_code >= 400:
+                last_error = HiveError(url + ' returned ' + str(query.status_code))
+
+                logging.error(str(last_error))
+
+                break
+        except requests.exceptions.HTTPError as error:
+            logging.warning(str(error))
+            logging.warning('Retrying in %.2f seconds...', initial_retry_duration)
+
+            last_error = error
+        except requests.exceptions.Timeout as error:
+            logging.warning(str(error))
+            logging.warning('Retrying in %.2f seconds...', initial_retry_duration)
+
+            last_error = error
+        except requests.exceptions.ConnectionError as error:
+            logging.warning(str(error))
+            logging.warning('Retrying in %.2f seconds...', initial_retry_duration)
+
+            last_error = error
+
+        time.sleep(initial_retry_duration)
+
+        initial_retry_duration *= 2
+
+    if last_error is not None:
+        raise last_error # pylint: disable=raising-bad-type
+
+    raise Exception('Unknown error occurred.')
+
+class VariableScope(Enum):
+    game = 'game'
+    player = 'player'
+    session = 'session'
+
+class HiveClient(object):
+    def __init__(self, **kwargs):
+        self.api_url = kwargs['api_url']
+        self.token = None
+        self.timeout = None
+
+        if self.api_url.endswith('/') is False:
+            self.api_url = self.api_url + '/'
+
+        if 'token' in kwargs:
+            self.token = kwargs['token']
+
+        if 'timeout' in kwargs:
+            self.timeout = kwargs['timeout']
+
+        if 'logger' in kwargs:
+            self.logger = kwargs['logger']
+        else:
+            self.logger = logging.getLogger()
+            self.logger.setLevel(logging.INFO)
+
+            handler = logging.StreamHandler(sys.stdout)
+            handler.setLevel(logging.INFO)
+            formatter = logging.Formatter('HiveMechanic Client [%(levelname)s] %(asctime)s: %(message)s')
+            handler.setFormatter(formatter)
+
+            self.logger.addHandler(handler)    
+
+    def issue_commands(self, commands, player=None, endpoint=None):
+        payload = {
+            'token': self.token,
+            'player': player
+        }
+
+        payload_commands = []
+
+        for command in commands:
+            payload_commands.append(command.to_dict())
+
+        payload['commands'] = json.dumps(payload_commands, indent=2)
+
+        response = post_request_with_retries(self.command_url(), payload, self.logger, server_timeout=self.timeout)
+
+        return response.json()
+
+    def issue_command(self, command, player=None):
+        return self.issue_commands([command], player)
+
+    def fetch_variable(self, variable_name, player=None, default=None, scope='game'):
+        payload = {
+            'token': self.token,
+            'name': variable_name,
+            'scope': scope
+        }
+        
+        if player is not None:
+            payload['player'] = player
+
+        response = post_request_with_retries(self.fetch_url(), payload, self.logger, server_timeout=self.timeout)
+
+        logging.info('%s = %s' % (self.fetch_url(), str(response.json())))
+        
+        if 'value' in response.json():
+            logging.info('%s = %s' % (variable_name, str(response.json()['value'])))
+            
+            return response.json()['value']
+
+        logging.info('Unable to retrieve value for %s from %s' % (variable_name, str(self.fetch_url())))
+        
+        return default
+
+    def command_url(self, endpoint='commands.json'):
+        return self.api_url + endpoint
+
+    def fetch_url(self, endpoint='fetch.json'):
+        return self.api_url + endpoint
+
+
+class Command(object):
+    def __init__(self, **kwargs):
+        pass
+
+    def to_dict(self):
+        command = {}
+
+        self.add_arguments(command)
+
+        command['type'] = self.command_type()
+
+        return command
+
+    def add_arguments(self, command):
+        pass # Add additional arguments in subclass.
+
+    def command_type(self):
+        return 'base-command'
+
+
+class GotoCommand(Command):
+    def __init__(self, destination, **kwargs):
+        super(GotoCommand, self).__init__(**kwargs)
+
+        self.destination = destination
+
+    def add_arguments(self, command):
+        command['destination'] = self.destination
+
+    def command_type(self):
+        return 'go-to'
+
+'''
+
+
+
+
+
+    def query_data_points(self, *args, **kwargs): # pylint: disable=unused-argument
+        # Add filter to recorded field so data set does not grow as data is added while querying...
+
+        now = arrow.utcnow().datetime
+
+        return PDKDataPointQuery(self.token, self.site_url, self.timeout, **kwargs).filter(recorded__lte=now)
+
+    def query_data_sources(self, *args, **kwargs): # pylint: disable=unused-argument
+        return PDKDataSourceQuery(self.token, self.site_url, self.timeout, **kwargs).exclude(pk=None)
+
+
+class PDKDataPointQuery(object): # pylint: disable=too-many-instance-attributes
+    def __init__(self, token, site_url, timeout, *args, **kwargs): # pylint: disable=unused-argument
+        self.token = token
+        self.site_url = site_url
+        self.timeout = timeout
+
+        page_size = PDK_API_DEFAULT_PAGE_SIZE
+
+        if 'page_size' in kwargs:
+            page_size = kwargs['page_size']
+            del kwargs['page_size']
+
+        self.page_size = page_size
+
+        self.filters = []
+        self.excludes = []
+        self.order_bys = []
+
+        if kwargs:
+            self.filters.append(kwargs)
+
+        self.total_count = None
+        self.page_index = 0
+        self.current_index = 0
+
+        self.current_page = None
+
+    def filter(self, **kwargs):
+        query = PDKDataPointQuery(self.token, self.site_url, self.page_size)
+
+        query.filters = list(self.filters)
+        query.excludes = list(self.excludes)
+        query.order_bys = list(self.order_bys)
+
+        query.filters.append(kwargs)
+
+        return query
+
+    def exclude(self, **kwargs):
+        query = PDKDataPointQuery(self.token, self.site_url, self.page_size)
+
+        query.filters = list(self.filters)
+        query.excludes = list(self.excludes)
+        query.order_bys = list(self.order_bys)
+
+        query.excludes.append(kwargs)
+
+        return query
+
+    def order_by(self, *args):
+        query = PDKDataPointQuery(self.token, self.site_url, self.page_size)
+
+        query.filters = list(self.filters)
+        query.excludes = list(self.excludes)
+        query.order_bys = list(self.order_bys)
+
+        query.order_bys.append(args)
+
+        return query
+
+    def count(self):
+        if self.total_count is not None:
+            return self.total_count
+
+        self.load_page(0)
+
+        return self.total_count
+
+    def __iter__(self):
+        self.load_page(0)
+
+        return self
+
+    def next(self):
+        if self.current_index >= self.total_count:
+            raise StopIteration
+
+        if self.current_index >= (self.page_index + 1) * self.page_size:
+            self.load_page(self.page_index + 1)
+
+        value = self.current_page[self.current_index % self.page_size]
+
+        self.current_index += 1
+
+        return value
+
+    def __getitem__(self, slice_item):
+        if isinstance(slice_item, (int, long)):
+            index = slice_item
+
+            if self.current_page is None:
+                self.load_page(0)
+
+            if index < 0:
+                index = self.total_count + index
+
+            if (index >= (self.page_index * self.page_size)) and (index < ((self.page_index + 1) * self.page_size)):
+                return self.current_page[index % self.page_size]
+
+            self.load_page(int(index / self.page_size))
+
+            return self.current_page[index % self.page_size]
+        elif isinstance(slice_item, slice):
+            logging.error('SLICE NOT YET SUPPORTED: %s', str(slice_item))
+
+        return []
+
+    def first(self):
+        return self[0]
+
+    def last(self):
+        return self[-1]
+
+    def load_page(self, page_number):
+        self.page_index = page_number
+
+        payload = {
+            'token': self.token,
+            'page_size': self.page_size,
+            'page_index': self.page_index,
+            'filters': json.dumps(self.filters, cls=DatetimeEncoder),
+            'excludes': json.dumps(self.excludes, cls=DatetimeEncoder),
+            'order_by': json.dumps(self.order_bys, cls=DatetimeEncoder),
+        }
+
+        fetch_page = post_request_with_retries(self.site_url + '/api/data-points.json', payload, server_timeout=self.timeout)
+
+        if fetch_page.status_code == requests.codes.ok:
+            response_payload = fetch_page.json()
+
+            self.total_count = response_payload['count']
+            self.page_index = response_payload['page_index']
+            self.page_size = response_payload['page_size']
+
+            self.current_page = response_payload['matches']
+        else:
+            fetch_page.raise_for_status()
+
+class DatetimeEncoder(json.JSONEncoder):
+    def default(self, obj): # pylint: disable=arguments-differ, method-hidden
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+
+        try:
+            return super(DatetimeEncoder, obj).default(obj)
+        except TypeError:
+            return str(obj)
+
+
+class PDKDataSourceQuery(object): # pylint: disable=too-many-instance-attributes
+    def __init__(self, token, site_url, timeout, *args, **kwargs): # pylint: disable=unused-argument
+        self.token = token
+        self.site_url = site_url
+        self.timeout = timeout
+
+        page_size = PDK_API_DEFAULT_PAGE_SIZE
+
+        if 'page_size' in kwargs:
+            page_size = kwargs['page_size']
+            del kwargs['page_size']
+
+        self.page_size = page_size
+
+        self.filters = []
+        self.excludes = []
+        self.order_bys = []
+
+        if kwargs:
+            self.filters.append(kwargs)
+
+        self.total_count = None
+        self.page_index = 0
+        self.current_index = 0
+
+        self.current_page = None
+
+    def filter(self, **kwargs):
+        query = PDKDataSourceQuery(self.token, self.site_url, self.page_size)
+
+        query.filters = list(self.filters)
+        query.excludes = list(self.excludes)
+        query.order_bys = list(self.order_bys)
+
+        query.filters.append(kwargs)
+
+        return query
+
+    def exclude(self, **kwargs):
+        query = PDKDataSourceQuery(self.token, self.site_url, self.page_size)
+
+        query.filters = list(self.filters)
+        query.excludes = list(self.excludes)
+        query.order_bys = list(self.order_bys)
+
+        query.excludes.append(kwargs)
+
+        return query
+
+    def order_by(self, *args):
+        query = PDKDataSourceQuery(self.token, self.site_url, self.page_size)
+
+        query.filters = list(self.filters)
+        query.excludes = list(self.excludes)
+        query.order_bys = list(self.order_bys)
+
+        query.order_bys.append(args)
+
+        return query
+
+    def count(self):
+        if self.total_count is not None:
+            return self.total_count
+
+        self.load_page(0)
+
+        return self.total_count
+
+    def __iter__(self):
+        self.load_page(0)
+
+        return self
+
+    def next(self):
+        if self.current_index >= self.total_count:
+            raise StopIteration
+
+        if self.current_index >= (self.page_index + 1) * self.page_size:
+            self.load_page(self.page_index + 1)
+
+        value = self.current_page[self.current_index % self.page_size]
+
+        self.current_index += 1
+
+        return value
+
+    def __getitem__(self, slice_item):
+        if isinstance(slice_item, (int, long)):
+            index = slice_item
+
+            if self.current_page is None:
+                self.load_page(0)
+
+            if index < 0:
+                index = self.total_count + index
+
+            if (index >= (self.page_index * self.page_size)) and (index < ((self.page_index + 1) * self.page_size)):
+                return self.current_page[index % self.page_size]
+
+            self.load_page(int(index / self.page_size))
+
+            return self.current_page[index % self.page_size]
+        elif isinstance(slice_item, slice):
+            logging.error('SLICE NOT YET SUPPORTED: %s', str(slice_item))
+
+        return []
+
+    def first(self):
+        return self[0]
+
+    def last(self):
+        return self[-1]
+
+    def load_page(self, page_number):
+        self.page_index = page_number
+
+        payload = {
+            'token': self.token,
+            'page_size': self.page_size,
+            'page_index': self.page_index,
+            'filters': json.dumps(self.filters, cls=DatetimeEncoder),
+            'excludes': json.dumps(self.excludes, cls=DatetimeEncoder),
+            'order_by': json.dumps(self.order_bys, cls=DatetimeEncoder),
+        }
+
+        fetch_page = post_request_with_retries(self.site_url + '/api/data-sources.json', payload, server_timeout=self.timeout)
+
+        if fetch_page.status_code == requests.codes.ok:
+            response_payload = fetch_page.json()
+
+            self.total_count = response_payload['count']
+            self.page_index = response_payload['page_index']
+            self.page_size = response_payload['page_size']
+
+            self.current_page = response_payload['matches']
+        else:
+            fetch_page.raise_for_status()
+'''
